baaf8514436bfc0fa22046b5ff5398969e8cecfe
yuliiahrabar <yuliia.hrabar@gmail.com>
2017-04-24 02:14:41 +0200

    Second part of filling table

--- main.cpp
+++ main.cpp
@@ -8,6 +8,12 @@
 
 using namespace std;
 
+struct  possibleDirections {
+	int x;
+	int y;
+	int direction;
+};
+
 // function for drawing the triangular lattice for the background
 void drawLattice(int L)
 {
@@ -360,6 +366,94 @@ int main()
 		}
 		concentration = needles_mass / (L * L);
 	}
-	pos_file.close();
+
+	/// Part two after filling half of table
+
+	vector<possibleDirections> posDir;
+
+	int idx = 0;
+	int row_start;
+	for (int i = 0; i < L; i = i++)
+	{
+		for (int j = 0; j < L; j++)
+		{
+			if ((j % 2 > 0 && i % 2 > 0) || (j % 2 == 0 && i % 2 == 0))
+			{
+
+				if (j % 2 == 0) // even 
+					row_start = 0;
+				else
+					row_start = 1;
+				if (check_direction(lattice, L, i, j, row_start, a, 1))
+				{
+					//Push back new subject created with default constructor
+					posDir.push_back(possibleDirections());
+					posDir[idx].x = i;
+					posDir[idx].y = j;
+					posDir[idx].direction = 1;
+					idx++;
+				}
+				if (check_direction(lattice, L, i, j, row_start, a, 2))
+				{
+					//Push back new subject created with default constructor
+					posDir.push_back(possibleDirections());
+					posDir[idx].x = i;
+					posDir[idx].y = j;
+					posDir[idx].direction = 2;
+					idx++;
+				}
+				if (check_direction(lattice, L, i, j, row_start, a, 3))
+				{
+					//Push back new subject created with default constructor
+					posDir.push_back(possibleDirections());
+					posDir[idx].x = i;
+					posDir[idx].y = j;
+					posDir[idx].direction = 3;
+					idx++;
+				}
+			}
+		}
+	}
+
+	/*for (int i = 0; i < posDir.size(); i++)
+	{
+		cout << posDir[i].x << " " << posDir[i].y << " " << posDir[i].direction << endl;
+	}*/
 	
+	int rand_idx, x, y, direction;
+	//while (!posDir.empty())
+	//{
+	cout << "Start" << endl;
+		rand_idx = (rand() % (posDir.size()));
+		x = posDir[rand_idx].x;
+		y = posDir[rand_idx].y;
+		direction = posDir[rand_idx].direction;
+		if (y % 2 == 0) 
+			row_start = 0;
+		else
+			row_start = 1;
+		direction = posDir[rand_idx].direction;
+		write_in_file(pos_file, lattice, L, x, y, row_start, a, direction);
+		/*posDir.erase(posDir.begin() + rand_idx);
+		for (int i = 0; i < posDir.size(); i++)
+		{
+			x = posDir[i].x;
+			y = posDir[i].y;
+			direction = posDir[i].direction;
+			if (check_direction(lattice, L, x, y, row_start, a, direction) == false)
+			{
+				posDir.erase(posDir.begin() + rand_idx);
+			}
+		}
+		*/
+
+	//}/*
+		/*
+		cout << "New: " << endl;
+		for (int i = 0; i < posDir.size(); i++)
+		{
+			cout << posDir[i].x << " " << posDir[i].y << " " << posDir[i].direction << endl;
+		}
+		*/
+	pos_file.close();
 }
